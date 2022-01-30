5b6e5f8249e9b284272c167ef5dba175a18fc0e5
tshvt <ienar.k3@gmail.com>
2022-01-30 23:59:14 +0500

    Add spaces

--- words.py
+++ words.py
@@ -2,9 +2,9 @@ words = "began then seem out oil in both was later year feet use hand show all h
         "begin has into sun that at land think found near mean not carry over be well example night keep how together "\
         "see away begin world let one went spell I world so be tell right made white number earth one last my has add "\
         "how eat miss give take why letter made then above food great for each study them end country stop work the " \
-        "adjust adjustment administration administrator admire admission admit adolescent adopt adult advance advanced"\
-        "advantage adventure advertising advice advise adviser advocate affair" \
-        "chemical chest chicken chief child childhood Chinese chip chocolate choice cholesterol Christian Christmas" \
+        "adjust adjustment administration administrator admire admission admit adolescent adopt adult advance advanced "\
+        "advantage adventure advertising advice advise adviser advocate affair " \
+        "chemical chest chicken chief child childhood Chinese chip chocolate choice cholesterol Christian Christmas " \
         "if between which often small find set light men next always we until show tree hand hear young feet could be "\
         "begin they up over with find leave move tell name time very girl new car no red any part begin head thing " \
         "also around night most its large example follow change way often which new name take girl never sea well " \
